4a43727dbf9f975f312ef3cd12cf716e73c8e14e
Jackson Weidmann <Jackson.weidmann@gmail.com>
2018-06-20 21:39:52 -0700

    added colorful project

---
+++ SVG_times_test/svg_maker.py
@@ -0,0 +1,39 @@
+import math
+start="<svg id=\"Layer_1\" data-name=\"Layer 1\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 520 520\">"
+end="</svg>"
+global out
+out=""
+
+multiplier=2
+totalPoints=100
+offset=210
+
+def drawStuff():
+  ppoints=[]
+  for i in range(totalPoints):
+      ppoints.append(drawPoint(200, i, totalPoints))
+  for i in range(len(ppoints)):
+    cur=ppoints[i]
+    nex=ppoints[math.floor(i*multiplier%len(ppoints))]
+    global out
+    out+="  "+line(cur[0],cur[1],nex[0],nex[1],i)+"\n"
+
+  return start+out+end
+def drawPoint(r, currentPoint, totalPoints):
+
+  theta = ((math.pi*2) / totalPoints);
+  angle = (theta * currentPoint);
+
+  x = (r * math.cos(angle));
+  y = (r * math.sin(angle));
+  return [x,y]
+def line(x1,y1,x2,y2,num):
+    return "<line x1=\""+str(x1+offset)+"\" y1=\""+str(y1+offset)+"\" x2=\""+str(x2+offset)+"\" y2=\""+str(y2+offset)+"\" style=\"fill: none;stroke: hsl("+str(num/totalPoints*360)+",100%,50%);stroke-miterlimit: 10;stroke-width: 1px\"/>"
+
+multi=input("multiplier: ")
+points=input("number of points: ")
+name=input("file name: ")
+multiplier=float(multi)
+totalPoints=int(points)
+line_file = open(name+".svg","w")
+line_file.write(drawStuff())
